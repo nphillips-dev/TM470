ba6ac2bae14f0929b7dc5d4203b64243f6707338
nphillips-dev <nphillips.developer@gmail.com>
2020-03-18 06:36:19 +0000

    login table migration altered

--- Areas/Identity/Data/TM470User.cs
+++ Areas/Identity/Data/TM470User.cs
@@ -9,5 +9,7 @@ namespace TM470.Areas.Identity.Data
     // Add profile data for application users by adding properties to the TM470User class
     public class TM470User : IdentityUser
     {
+        public string bookVersion { get; set; }
+        public string friendId { get; set; }
     }
 }
---
+++ Migrations/20200318063315_User_bookVersion_friendID.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TM470.Migrations
+{
+    public partial class User_bookVersion_friendID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "bookVersion",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "friendId",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "bookVersion",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "friendId",
+                table: "AspNetUsers");
+        }
+    }
+}
